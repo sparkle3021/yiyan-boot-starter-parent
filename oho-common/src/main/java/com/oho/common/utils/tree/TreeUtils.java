96795624334f2c8a444af4ecb08de0ef5b21bca6
MENGJIAO <mengjiao.cn@outlook.com>
2023-05-05 11:39:40 +0800

    feat: 添加工具类
    1. 数组，集合转树工具类
    2. 常用排序工具类

--- oho-common/src/main/java/com/oho/common/utils/tree/TreeUtils.java
+++ oho-common/src/main/java/com/oho/common/utils/tree/TreeUtils.java
@@ -1,52 +1,91 @@
 package com.oho.common.utils.tree;
 
-import java.util.*;
+import cn.hutool.core.lang.tree.Tree;
+
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * 树工具类
  *
- * @author baiyan
- * @date 2020/11/13
+ * @author MENGJIAO
+ * @createDate 2023-05-05 10:11
  */
 public class TreeUtils {
 
-    private TreeUtils() {
+    /**
+     * Iterator转树
+     */
+    public static <T extends Tree<T>> T buildTree(T root, Iterator<T> nodes) {
+        if (root == null) {
+            return null;
+        }
+        while (nodes.hasNext()) {
+            T node = nodes.next();
+            if (root.getId().equals(node.getParentId())) {
+                root.getChildren().add(buildTree(node, nodes));
+            }
+        }
+        return root;
     }
 
     /**
-     * 生成一颗树
-     * 利用treeFactory将source转成树节点
-     * 树节点继承Treeable
-     *
-     * @param source
-     * @param root        根节点
-     * @param treeFactory 将source转成树节点，生成默认节点
-     * @param <I>
-     * @param <O>
-     * @return
+     * Iterable转树
      */
-    public static <I, O extends Treeable> List<O> buildTree(List<I> source, I root, TreeFactory<I, O> treeFactory) {
-        Map<Object, O> mapping = new LinkedHashMap<>();
-        O treeRoot;
-        // 如果没有root就生成一个默认节点
-        if (Objects.isNull(root)) {
-            treeRoot = treeFactory.buildDefaultRoot();
-        } else {
-            treeRoot = treeFactory.convert(root);
+    public static <T extends Tree<T>> T buildTree(T root, Iterable<T> nodes) {
+        if (root == null) {
+            return null;
         }
-        mapping.put(treeRoot.key(), treeRoot);
-
-        source.stream()
-                .forEach(i -> {
-                    O o = treeFactory.convert(i);
-                    mapping.putIfAbsent(o.key(), o);
-                });
-
-        mapping.forEach((key, value) -> Optional.ofNullable(value)
-                .filter(t -> !Objects.equals(treeRoot, value))
-                .map(t -> mapping.get(t.parentKey()))
-                .ifPresent(parent -> parent.add(value)));
-        return treeRoot.children();
+        for (T node : nodes) {
+            if (root.getId().equals(node.getParentId())) {
+                root.getChildren().add(buildTree(node, nodes));
+            }
+        }
+        return root;
     }
 
+    /**
+     * 数组转树
+     */
+    public static <T extends Tree<T>> T buildTree(T root, T[] nodes) {
+        if (root == null) {
+            return null;
+        }
+        for (T node : nodes) {
+            if (root.getId().equals(node.getParentId())) {
+                root.getChildren().add(buildTree(node, nodes));
+            }
+        }
+        return root;
+    }
+
+    /**
+     * List转树
+     */
+    public static <T extends Tree<T>> T buildTree(T root, List<T> nodes) {
+        if (root == null) {
+            return null;
+        }
+        for (T node : nodes) {
+            if (root.getId().equals(node.getParentId())) {
+                root.getChildren().add(buildTree(node, nodes));
+            }
+        }
+        return root;
+    }
+
+    /**
+     * Set转树
+     */
+    public static <T extends Tree<T>> T buildTree(T root, java.util.Set<T> nodes) {
+        if (root == null) {
+            return null;
+        }
+        for (T node : nodes) {
+            if (root.getId().equals(node.getParentId())) {
+                root.getChildren().add(buildTree(node, nodes));
+            }
+        }
+        return root;
+    }
 }
