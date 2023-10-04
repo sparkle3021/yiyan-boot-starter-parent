16bfa01828ab075af21213333763ca6718992abf
MENGJIAO <mengjiao.cn@outlook.com>
2023-10-05 02:29:30 +0800

    fix: 修正mybatis模块内Mapper无法识别的问题

--- yiyan-boot-starter-mybatis/src/main/java/com/yiyan/boot/mybatis/autoconfigure/MyBatisPlusAutoConfiguration.java
+++ yiyan-boot-starter-mybatis/src/main/java/com/yiyan/boot/mybatis/autoconfigure/MyBatisPlusAutoConfiguration.java
@@ -1,10 +1,14 @@
 package com.yiyan.boot.mybatis.autoconfigure;
 
+import com.yiyan.boot.mybatis.core.model.constant.ConfigKey;
 import lombok.extern.slf4j.Slf4j;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * @author MENGJIAO
@@ -12,8 +16,27 @@ import org.springframework.context.annotation.Configuration;
 @Slf4j
 @Configuration
 @ConditionalOnProperty(value = "mybatis-plus.type-aliases-package")
-@ComponentScan("com.oho.mybatis")
-@MapperScan("com.oho.mybatis.core.mapper")
 public class MyBatisPlusAutoConfiguration {
+    /**
+     * mybatis-plus批量操作线程池
+     */
+    @Bean(name = "mybatisBatchExecutor")
+    public Executor asyncServiceExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        //配置核心线程数
+        executor.setCorePoolSize(ConfigKey.MAX_BATCH_THREAD_SIZE);
+        //配置最大线程数
+        executor.setMaxPoolSize(ConfigKey.MAX_BATCH_THREAD_SIZE);
+        //配置队列大小
+        executor.setQueueCapacity(ConfigKey.MAX_BATCH_THREAD_SIZE * 4);
+        //配置线程池中的线程的名称前缀
+        executor.setThreadNamePrefix("MYBATIS-BATCH-");
 
+        // rejection-policy：当pool已经达到max size的时候，如何处理新任务
+        // CALLER_RUNS：不在新线程中执行任务，而是有调用者所在的线程来执行
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        //执行初始化
+        executor.initialize();
+        return executor;
+    }
 }
